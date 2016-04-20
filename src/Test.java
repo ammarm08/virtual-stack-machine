9833a8cfece08d545893a47ddb8ba5674c1fa2e9
ammarm08 <ammar.mian08@gmail.com>
2016-04-20 12:37:33 -0400

    add test to test class

--- src/Test.java
+++ src/Test.java
@@ -5,10 +5,11 @@ import static vm.Bytecode.*;
 public class Test {
 
   static int[] hello {
-    HALT
+    HALT;
   };
 
   public static void main(String args[]) {
-
+    VM vm = new VM(hello, 0, 0); // bytecode[], ip, datasize
+    vm.cpu();
   }
 }
