3f1eb43e2cb3576f62d2cb2215205f62e99d63ee
ammarm08 <ammar.mian08@gmail.com>
2016-04-20 12:30:05 -0400

    add vm class to mock a vm

---
+++ src/VM.java
@@ -0,0 +1,24 @@
+package vm;
+
+public class VM {
+  int[] data; // data memory
+  int[] code; // code memory
+  int[] stack; // call stack
+
+  // registers
+  int ip; // instruction pointer
+  int sp = -1; // stack pointer starts neg to offset for first instruction
+  int fp; // frame pointer
+
+  // constructor
+  public VM(int[] code, int main, int datasize) {
+    this.code = code;
+    this.ip = main;
+    this.data = new int[datasize]; // init size of data memory
+    this.stack = new int[100]; // arbitrary size for call stack
+  }
+
+  public void cpu() {
+    // do nothing 
+  }
+}
